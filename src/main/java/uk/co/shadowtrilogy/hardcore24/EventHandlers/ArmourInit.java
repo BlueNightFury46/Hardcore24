b6ea63390486d1b76623c54dc7277890a80f50a5
BlueNightFury46 <loranlatias@gmail.com>
2024-02-27 16:52:57 +0000

    fixed null exceptions and disabled the mob-armour override

--- src/main/java/uk/co/shadowtrilogy/hardcore24/EventHandlers/ArmourInit.java
+++ src/main/java/uk/co/shadowtrilogy/hardcore24/EventHandlers/ArmourInit.java
@@ -2,6 +2,7 @@ package uk.co.shadowtrilogy.hardcore24.EventHandlers;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ArmourInit {
 
@@ -16,7 +17,29 @@ public class ArmourInit {
     public static final ItemStack bloodMoon3 = new ItemStack(Material.NETHERITE_HELMET);
     public static final ItemStack bloodMoon4 = new ItemStack(Material.NETHERITE_HELMET);
 
+    public static final ItemStack none = new ItemStack(Material.AIR);
+
     public static void init(){
+           HarderInit();
+
+    }
+
+    public static void HarderInit(){
+        ItemMeta d = diamond.getItemMeta();
+        ItemMeta i1 = iron1.getItemMeta();
+        ItemMeta i2 = iron2.getItemMeta();
+        ItemMeta i3 = iron3.getItemMeta();
+
+        d.setDisplayName("{Hardcore24:Exclusive}DoNotObtainOrModify?.23");
+        i1.setDisplayName("{Hardcore24:Exclusive}DoNotObtainOrModify?.24");
+        i2.setDisplayName("{Hardcore24:Exclusive}DoNotObtainOrModify?.25");
+        i3.setDisplayName("{Hardcore24:Exclusive}DoNotObtainOrModify?.26");
+
+        diamond.setItemMeta(d);
+        iron1.setItemMeta(i1);
+        iron2.setItemMeta(i2);
+        iron3.setItemMeta(i3);
+
 
     }
 
--- src/main/java/uk/co/shadowtrilogy/hardcore24/EventHandlers/MoreMobs.java
+++ src/main/java/uk/co/shadowtrilogy/hardcore24/EventHandlers/MoreMobs.java
@@ -22,6 +22,9 @@ public class MoreMobs implements Listener {
 
 
     public boolean bloodmoon = false;
+    public boolean removeArmour = false;
+
+    int phaseCounter = 0;
 
     @EventHandler
     public void events(EntitySpawnEvent event){
@@ -30,72 +33,152 @@ public class MoreMobs implements Listener {
         World world_hardcore = Bukkit.getWorld(Hardcore24.plugin.getConfig().getString("hardcore-world.hardcore-normal").toString());
 
 
-           if (world_hardcore.getFullTime() < 23000 && world_hardcore.getFullTime() > 13000 && bool == true) {
+           if (world_hardcore.getFullTime() < 23000 && world_hardcore.getFullTime() > 13000) {
                long phase = world_hardcore.getFullTime() / 24000;
                if (phase == 0) {
 
 
+
+
                    if(Hardcore24.playOnce == true) {
 
-                       for(Player player : Bukkit.getOnlinePlayers()){
-                           player.sendMessage(ChatColor.DARK_RED + "As the full moon rises the countless monsters band together,\nyou encounter mobs with stronger gear...");
-                           world_hardcore.playSound(player.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1.2f, 1.0f);
 
+                       for(Player player : Bukkit.getOnlinePlayers()){
+                           if(bool) {
+                               player.sendMessage(ChatColor.DARK_RED + "As the full moon rises the countless monsters band together,\nyou encounter mobs with stronger gear...");
+                               world_hardcore.playSound(player.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1.2f, 1.0f);
+                           }
                            if(Moon == true) {
                                int i = ThreadLocalRandom.current().nextInt(0, 4);
                                if (i == 3) {
+                                   player.sendMessage(ChatColor.DARK_RED + "As the blood moon rises the countless monsters band together,\nyou encounter mobs with stronger gear...");
+                                   world_hardcore.playSound(player.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1.2f, 1.0f);
                                    bloodmoon = true;
                                }
                            }
                        }
+                       if(bloodmoon = true){
+                           //if we have another blood moon reset the counter
+                           if(phaseCounter!=0){
+                               phaseCounter = 0;
+                           //otherwise we set the counter to 1
+                           } else if (phaseCounter == 0) {
+                               phaseCounter = 1;
+                           }
+                       }
+                       if(phaseCounter == 1) {
+                           removeArmour = true;
+                           bloodmoon = false;
+                       }
+
                        Hardcore24.playOnce = false;
                    }
 
-                   for (LivingEntity entity : world_hardcore.getLivingEntities()) {
-                       if (entity.getType().equals(EntityType.SKELETON) || entity.getType().equals(EntityType.ZOMBIE))
 
-                           if(bloodmoon == true){
+
+                   for (LivingEntity entity : world_hardcore.getLivingEntities()) {
+                       if (entity.getType().equals(EntityType.SKELETON) && !Bukkit.getOnlinePlayers().contains(entity) || entity.getType().equals(EntityType.ZOMBIE) && !Bukkit.getOnlinePlayers().contains(entity)) {
+
+
+
+                           if (removeArmour == true) {
+                               //Removes Blood moon helmet
+                               if (!entity.getEquipment().getHelmet().getType().equals(Material.AIR)) {
+                                   if (entity.getEquipment().getHelmet().isSimilar(ArmourInit.bloodMoon1)) {
+
+                                   }
+                               }
+                               //Removes Blood moon Chestplate
+                               if (!entity.getEquipment().getHelmet().getType().equals(Material.AIR)) {
+                                   if (entity.getEquipment().getHelmet().isSimilar(ArmourInit.bloodMoon1)) {
+
+                                   }
+                               }
+                           }
+
+
+                           if (bloodmoon == true) {
+                               //Checks if entity already has a helmet and if not sets the helmet to Netherite
+                               if (entity.getEquipment().getHelmet().getType().equals(Material.AIR)) {
+                                   entity.getEquipment().setHelmet(ArmourInit.bloodMoon1);
+                                   entity.getEquipment().getHelmet().addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                                   entity.getEquipment().setHelmetDropChance(0.0f);
+                               }
+                               //Checks if entity already has a chestplate and if not sets the chestplate to Netherite
+                               if (entity.getEquipment().getChestplate().getType().equals(Material.AIR)) {
+                                   entity.getEquipment().setChestplate(ArmourInit.bloodMoon2);
+                               }
+                               //Checks if entity already has leggings and if not sets the leggings to Netherite
+                               if (entity.getEquipment().getLeggings().getType().equals(Material.AIR)) {
+                                   entity.getEquipment().setLeggings(ArmourInit.bloodMoon3);
+                               }
+                               //Checks if entity already has boots and if not sets the boots to Netherite
+                               if (entity.getEquipment().getBoots().getType().equals(Material.AIR)) {
+                                   entity.getEquipment().setBoots(ArmourInit.bloodMoon4);
+                               }
+                           } else if (bool == true) {
                                //Checks if entity already has a helmet and if not sets the helmet to diamond
-                               if(entity.getEquipment().getHelmet()==null) {
+                               if (entity.getEquipment().getHelmet().getType().equals(Material.AIR)) {
                                    entity.getEquipment().setHelmet(ArmourInit.diamond);
                                }
                                //Checks if entity already has a chestplate and if not sets the chestplate to iron
-                               if(entity.getEquipment().getChestplate()==null) {
+                               if (entity.getEquipment().getChestplate().getType().equals(Material.AIR)) {
                                    entity.getEquipment().setChestplate(ArmourInit.iron1);
                                }
                                //Checks if entity already has leggings and if not sets the leggings to iron
-                               if(entity.getEquipment().getLeggings()==null) {
+                               if (entity.getEquipment().getLeggings().getType().equals(Material.AIR)) {
                                    entity.getEquipment().setLeggings(ArmourInit.iron2);
                                }
                                //Checks if entity already has boots and if not sets the boots to iron
-                               if(entity.getEquipment().getBoots()==null) {
-                                   entity.getEquipment().setBoots(ArmourInit.iron3);
-                               }
-                           } else if (bool){
-                               //Checks if entity already has a helmet and if not sets the helmet to diamond
-                               if(entity.getEquipment().getHelmet()==null) {
-                                   entity.getEquipment().setHelmet(ArmourInit.diamond);
-                               }
-                               //Checks if entity already has a chestplate and if not sets the chestplate to iron
-                               if(entity.getEquipment().getChestplate()==null) {
-                                   entity.getEquipment().setChestplate(ArmourInit.iron1);
-                               }
-                               //Checks if entity already has leggings and if not sets the leggings to iron
-                               if(entity.getEquipment().getLeggings()==null) {
-                                   entity.getEquipment().setLeggings(ArmourInit.iron2);
-                               }
-                               //Checks if entity already has boots and if not sets the boots to iron
-                               if(entity.getEquipment().getBoots()==null) {
+                               if (entity.getEquipment().getBoots().getType().equals(Material.AIR)) {
                                    entity.getEquipment().setBoots(ArmourInit.iron3);
                                }
                            }
 
 
+                       }
                    }
                }
 
 
        }
+           else if(world_hardcore.getFullTime() < 12500){
+               Hardcore24.playOnce = true;
+
+               //Remove HarderMobs armour
+               for(LivingEntity entity : world_hardcore.getLivingEntities()){
+                   //Remove helmet if helmet exists & is the correct helmet
+                   if(!entity.getEquipment().getHelmet().getType().equals(Material.AIR)) {
+                       if(entity.getEquipment().getHelmet().isSimilar(ArmourInit.diamond))
+                       {
+                           entity.getEquipment().setHelmet(ArmourInit.none);
+                       }
+                   }
+                  //Remove Chestplate
+                   if(!entity.getEquipment().getChestplate().getType().equals(Material.AIR)) {
+                       if(entity.getEquipment().getChestplate().isSimilar(ArmourInit.iron1))
+                       {
+                           entity.getEquipment().setChestplate(ArmourInit.none);
+                       }
+                   }
+
+                   //Remove Leggings
+                   if(!entity.getEquipment().getLeggings().getType().equals(Material.AIR)) {
+                       if(entity.getEquipment().getLeggings().isSimilar(ArmourInit.iron2))
+                       {
+                           entity.getEquipment().setLeggings(ArmourInit.none);
+                       }
+                   }
+
+                   //Remove Boots
+                   if(!entity.getEquipment().getBoots().getType().equals(Material.AIR)) {
+                       if(entity.getEquipment().getBoots().isSimilar(ArmourInit.bloodMoon1))
+                       {
+                           entity.getEquipment().setBoots(ArmourInit.none);
+                       }
+                   }
+               }
+           }
     }
 
 
